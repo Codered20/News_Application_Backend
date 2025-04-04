691d58d0b3f87e38924099a3ecb8ee35bf9a0f73
Anuj <Anuj201982@gmail.com>
2025-04-05 00:17:19 +0530

    Add retry logic

--- src/main/java/News/Backend/Api/Controllers/NewsController.java
+++ src/main/java/News/Backend/Api/Controllers/NewsController.java
@@ -1,5 +1,9 @@
 package News.Backend.Api.Controllers;
 
+import java.util.*;
+
+import org.springframework.http.HttpStatus;
+
 //import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -15,36 +19,79 @@ import org.springframework.web.client.RestTemplate;
 @CrossOrigin
 public class NewsController {
 	
-//	String api_Keys[] = {
-//			"2-rOj8u_H5Ku5dZTSaI7QlxVNwmxzMtRudKQzF5IULNYAA5r",
-//			"03FtBRp58cqjxXbQeuFymPSj0Qjm5maGM8zZKKWUG037TNon"
-//	};
-	String api_Key = "jWR3FV3lxi2cn5mwWgH8QcrUMX3dFRbdu4IaxcHE6ewYpOeJ";
+	String api_Keys[] = {
+			"jWR3FV3lxi2cn5mwWgH8QcrUMX3dFRbdu4IaxcHE6ewYpOeJ",
+			"2-rOj8u_H5Ku5dZTSaI7QlxVNwmxzMtRudKQzF5IULNYAA5r",
+			"03FtBRp58cqjxXbQeuFymPSj0Qjm5maGM8zZKKWUG037TNon",
+			
+	};
 	
 	@GetMapping("/getNews")
 	public ResponseEntity<?> getNews() {
-		String uri = "https://api.currentsapi.services/v1/latest-news?apiKey="+api_Key;
-		RestTemplate rest = new RestTemplate();
-		String response = rest.getForObject(uri, String.class);
-		return ResponseEntity.ok(response);
+		ResponseEntity<String> response = null;
+		for(String apiKey:api_Keys) {
+			try {
+				String uri = "https://api.currentsapi.services/v1/latest-news?apiKey="+apiKey;
+				RestTemplate rest = new RestTemplate();
+				response = rest.getForEntity(uri, String.class);
+				if(response.getStatusCode()== HttpStatus.OK) {
+					break;
+				}
+				System.out.println(response.getStatusCode());
+			}catch(Exception ex){
+				System.out.println(apiKey);
+				System.out.println(response);
+			}
+		}
+		return ResponseEntity.ok(response.getBody());
 	}
 	
 	@GetMapping("/getNews/category/{category}")
 	public ResponseEntity<?> getNewsByCategory(@PathVariable String category) {
-		String uri = "https://api.currentsapi.services/v1/latest-news?"+"apiKey="+api_Key+"&category=" + category+"&page_size=50";
-		System.out.println("Category");
-		RestTemplate rest = new RestTemplate();
-		String response = rest.getForObject(uri, String.class);
-		return ResponseEntity.ok(response);
+		ResponseEntity<String> response = null;
+		for(String apiKey:api_Keys) {
+			try {
+				String uri = "https://api.currentsapi.services/v1/latest-news?"+"apiKey="+apiKey+"&category=" + category+"&page_size=50";
+				RestTemplate rest = new RestTemplate();
+				response = rest.getForEntity(uri, String.class);
+				if(response.getStatusCode()== HttpStatus.OK) {
+					break;
+				}
+				System.out.println(response.getStatusCode());
+			}catch(Exception ex){
+				System.out.println("Limit Reached for "+apiKey);
+				System.out.println(response);
+			}
+		}
+		return ResponseEntity.ok(response.getBody());
 	}
 	
 	@GetMapping("/getNews/keyword/{keyword}")
 	public ResponseEntity<?> getNewsByKeyword(@PathVariable String keyword) {
-		String uri = "https://api.currentsapi.services/v1/search?keywords="+keyword+"&apiKey="+api_Key;
-		System.out.println("keyword");
-		RestTemplate rest = new RestTemplate();
-		String response = rest.getForObject(uri, String.class);
-		return ResponseEntity.ok(response);
+		String keys[] = {"3049b29cd5df779b9348285646be9d39",
+		                 "30f6c4f735bc87cbdb4ebf721128c4f9"};
+		ResponseEntity<String> response = null;
+		Map<String, Object> wrapper = null;
+		for(String key:keys) {
+			try {
+				String uri = "http://api.mediastack.com/v1/news"
+						+ "?access_key="+key
+						+ "&keywords="+keyword;
+				System.out.println("keyword");
+				RestTemplate rest = new RestTemplate();
+				response = rest.getForEntity(uri, String.class);
+				if(response.getStatusCode()==HttpStatus.OK) {
+					wrapper = new HashMap<>();
+				    wrapper.put("news", response.getBody());
+				    wrapper.put("status", "ok");
+				    break;
+				}
+			}catch(Exception ex) {
+				System.out.println("Limit reached for "+key);
+			}
+			
+		}
+		return ResponseEntity.ok(wrapper);
 	}
 	
 }
